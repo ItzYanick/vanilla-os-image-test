1d1f1f05ed3174c3c7e2319c1f85c8fb31a0ffac
Yanick Stephan <me@yanick.gay>
2024-12-23 17:40:06 +0100

    test commit

---
+++ includes.container/pkgs/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# AppImage Support
+# wget https://files.yanick.gay/os/linux/vanillaos/appimagelauncher_2.2.0_amd64.deb -P /pkgs
+
+# DisplayLink Support
+wget https://files.yanick.gay/os/linux/vanillaos/synaptics-repository-keyring.deb -P /pkgs
---
+++ includes.container/pkgs/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# AppImage Support
+apt-get install -y libfuse2t64
+# apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
+
+# DisplayLink Support
+apt-get install -y /pkgs/synaptics-repository-keyring.deb
