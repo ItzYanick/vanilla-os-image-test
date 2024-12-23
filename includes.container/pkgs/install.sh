6487220dff93c0d7b3e6caf06525d9f984617768
Yanick Stephan <me@yanick.gay>
2024-12-23 19:02:51 +0100

    try to use synaptics repo

--- includes.container/pkgs/install.sh
+++ includes.container/pkgs/install.sh
@@ -6,6 +6,9 @@ apt-get install -y libfuse2t64
 # apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
 
 # DisplayLink Support
-apt-get install -y dkms libdrm-dev linux-headers-$(uname -r)
-unzip /pkgs/DisplayLink_6.1_amd64.zip -d /pkgs
-SYSTEMINITDAEMON=systemd bash /pkgs/displaylink-driver-6.1.0-17.run
+apt-get install -y dkms libdrm-dev
+# unzip /pkgs/DisplayLink_6.1_amd64.zip -d /pkgs
+# SYSTEMINITDAEMON=systemd bash /pkgs/displaylink-driver-6.1.0-17.run
+apt-get install -y /pkgs/synaptics-repository-keyring.deb
+apt-get update
+apt-get install displaylink-driver
