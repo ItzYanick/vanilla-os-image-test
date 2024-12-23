404654beb34fcb5f1212e82b47f2346578217715
Yanick Stephan <me@yanick.gay>
2024-12-23 17:50:20 +0100

    fix typo... i hate myself

--- includes.container/pkgs/install.sh
+++ includes.container/pkgs/install.sh
@@ -6,6 +6,6 @@ apt-get install -y libfuse2t64
 # apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
 
 # DisplayLink Support
-apt-get install -y /pkgs/synaptics-repository-keyring.deb#
+apt-get install -y /pkgs/synaptics-repository-keyring.deb
 apt-get update
 apt-get install -y displaylink-driver
