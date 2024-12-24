ca3fb60b3f05076dd8a963f6d46294c9743b314b
Yanick <me@yanick.gay>
2024-12-24 14:36:58 +0100

    Update install.sh

--- includes.container/pkgs/install.sh
+++ includes.container/pkgs/install.sh
@@ -6,7 +6,7 @@ apt-get install -y libfuse2t64
 # apt-get install -y /pkgs/appimagelauncher_2.2.0_amd64.deb
 
 # DisplayLink Support
-apt-get install -y dkms libdrm-dev
+apt-get install -y dkms libdrm-dev linux-source
 # unzip /pkgs/DisplayLink_6.1_amd64.zip -d /pkgs
 # SYSTEMINITDAEMON=systemd bash /pkgs/displaylink-driver-6.1.0-17.run
 # apt-get install -y /pkgs/synaptics-repository-keyring.deb
